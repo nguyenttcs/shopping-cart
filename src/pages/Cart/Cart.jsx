164344312a31b7791f4eee701924b897f8875536
IrisNguyen <nguyentt.ir@gmail.com>
2020-04-19 14:55:52 +0700

    init UI cart list

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,6 +1,29 @@
 import React, { Component } from "react";
 
 class Cart extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      cartList: [
+        {
+          "id": "1",
+          "name": "Cake",
+          "image": "https://raw.githubusercontent.com/nguyenttcs/shopping-cart/0244645a7ca09d9d81aa481a5628c82872e47b32/src/assets/images/cake.jpg",
+          "price": 1000,
+          "quantity": 1 
+      },
+      {
+          "id": "2",
+          "name": "Coke",
+          "image": "https://github.com/nguyenttcs/shopping-cart/blob/0244645a7ca09d9d81aa481a5628c82872e47b32/src/assets/images/coke.jpg?raw=true",
+          "price": 100,
+          "quantity": 5 
+      }
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="cart-page">
@@ -8,80 +31,33 @@ class Cart extends Component {
           {/* Title */}
           <div className="title">Shopping Bag</div>
           {/* Product #1 */}
-          <div className="item">
-            <div className="buttons">
-              <span className="delete-btn" />
-              <span className="like-btn" />
-            </div>
-            <div className="image">
-              <img src="https://designmodo.com/demo/shopping-cart/item-1.png" alt="" />
-            </div>
-            <div className="description">
-              <span>Common Projects</span>
-              <span>Bball High</span>
-              <span>White</span>
-            </div>
-            <div className="quantity">
-              <button className="plus-btn" type="button" name="button">
-                <img src="https://designmodo.com/demo/shopping-cart/plus.svg" alt="" />
-              </button>
-              <input type="text" name="name" defaultValue={1} />
-              <button className="minus-btn" type="button" name="button">
-                <img src="https://designmodo.com/demo/shopping-cart/minus.svg" alt="" />
-              </button>
-            </div>
-            <div className="total-price">$549</div>
-          </div>
-          {/* Product #2 */}
-          <div className="item">
-            <div className="buttons">
-              <span className="delete-btn" />
-              <span className="like-btn" />
-            </div>
-            <div className="image">
-              <img src="https://designmodo.com/demo/shopping-cart/item-2.png" alt="" />
-            </div>
-            <div className="description">
-              <span>Maison Margiela</span>
-              <span>Future Sneakers</span>
-              <span>White</span>
-            </div>
-            <div className="quantity">
-              <button className="plus-btn" type="button" name="button">
-                <img src="https://designmodo.com/demo/shopping-cart/plus.svg" alt="" />
-              </button>
-              <input type="text" name="name" defaultValue={1} />
-              <button className="minus-btn" type="button" name="button">
-                <img src="https://designmodo.com/demo/shopping-cart/minus.svg" alt="" />
-              </button>
-            </div>
-            <div className="total-price">$870</div>
-          </div>
-          {/* Product #3 */}
-          <div className="item">
-            <div className="buttons">
-              <span className="delete-btn" />
-              <span className="like-btn" />
-            </div>
-            <div className="image">
-              <img src="https://designmodo.com/demo/shopping-cart/item-3.png" alt="" />
-            </div>
-            <div className="description">
-              <span>Our Legacy</span>
-              <span>Brushed Scarf</span>
-              <span>Brown</span>
-            </div>
-            <div className="quantity">
-              <button className="plus-btn" type="button" name="button">
-                <img src="https://designmodo.com/demo/shopping-cart/plus.svg" alt="" />
-              </button>
-              <input type="text" name="name" defaultValue={1} />
-              <button className="minus-btn" type="button" name="button">
-                <img src="https://designmodo.com/demo/shopping-cart/minus.svg" alt="" />
-              </button>
-            </div>
-            <div className="total-price">$349</div>
-          </div>
+          {
+            this.state.cartList.map((cartItem, index) => {
+              return (
+                <div className="item" key={index}>
+                  <div className="buttons">
+                    <span className="delete-btn" />
+                  </div>
+                  <div className="image">
+                    <img src={cartItem.image} alt="" />
+                  </div>
+                  <div className="description">
+                    <h4>{cartItem.name}</h4>
+                  </div>
+                  <div className="quantity">
+                    <button className="plus-btn" type="button" name="button">
+                      <img src="https://designmodo.com/demo/shopping-cart/plus.svg" alt="" />
+                    </button>
+                    <input type="text" name="name" defaultValue={1} value={cartItem.quantity} />
+                    <button className="minus-btn" type="button" name="button">
+                      <img src="https://designmodo.com/demo/shopping-cart/minus.svg" alt="" />
+                    </button>
+                  </div>
+              <div className="total-price">${cartItem.price}</div>
+                </div>
+              )
+            })
+          }
         </div>
       </div>
     );
